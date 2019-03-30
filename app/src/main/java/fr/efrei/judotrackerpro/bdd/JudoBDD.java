9764ed4d8d52a17a7c968c4192ccc4d40a514925
Kilian Chollet <chollet.kilian@gmail.com>
2019-03-30 16:39:12 +0100

    JudoBDD erreur d'import

--- app/src/main/java/fr/efrei/judotrackerpro/bdd/JudoBDD.java
+++ app/src/main/java/fr/efrei/judotrackerpro/bdd/JudoBDD.java
@@ -4,7 +4,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-import judotrackerpro.*;
+import fr.efrei.judotrackerpro.entities.*;
 
 
 public class JudoBDD {
