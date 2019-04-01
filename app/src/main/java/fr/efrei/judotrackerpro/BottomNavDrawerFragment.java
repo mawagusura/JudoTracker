38adb798ebfb025518e47fc3ab282816b24dfcc4
mawagusura <mawagusura@gmail.com>
2019-04-01 18:19:36 +0200

    2 firsts activities

---
+++ app/src/main/java/fr/efrei/judotrackerpro/BottomNavDrawerFragment.java
@@ -0,0 +1,27 @@
+package fr.efrei.judotrackerpro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class BottomNavDrawerFragment extends BottomSheetDialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.bottom_nav_drawer_fragment, container, false);
+
+        NavigationView nav_view = view.findViewById(R.id.nav_view);
+
+        return view;
+    }
+}
--- app/src/main/java/fr/efrei/judotrackerpro/MainActivity.java
+++ app/src/main/java/fr/efrei/judotrackerpro/MainActivity.java
@@ -1,13 +1,42 @@
 package fr.efrei.judotrackerpro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomAppBar bottomAppBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        setSupportActionBar(bottomAppBar);
+
+
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case android.R.id.home :
+                BottomNavDrawerFragment b = new BottomNavDrawerFragment();
+                b.show(getSupportFragmentManager(), b.getTag());
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
