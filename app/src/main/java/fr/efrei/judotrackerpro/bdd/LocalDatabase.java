0047c4e079b927cc89841166e3bf38361daf8afc
Kilian Chollet <chollet.kilian@gmail.com>
2019-04-06 19:23:27 +0200

    Ajout fonctions database

--- app/src/main/java/fr/efrei/judotrackerpro/bdd/LocalDatabase.java
+++ app/src/main/java/fr/efrei/judotrackerpro/bdd/LocalDatabase.java
@@ -5,13 +5,16 @@ import android.content.Context;
 import java.util.List;
 
 import androidx.room.Room;
+import fr.efrei.judotrackerpro.entities.Adversaire;
+import fr.efrei.judotrackerpro.entities.Categorie;
 import fr.efrei.judotrackerpro.entities.Competition;
+import fr.efrei.judotrackerpro.entities.Localisation;
+import fr.efrei.judotrackerpro.entities.Match;
+import fr.efrei.judotrackerpro.entities.Statistiques;
 
 public class LocalDatabase {
 
-    // J'implémenterai ici tout le nécessaire pour récupérer les données dans les vues (pour pas interagir avec le DAOs direct dans les activity)
-
-    ConnectorDB bdd;
+    private static ConnectorDB bdd;
 
     public LocalDatabase(Context context) {
         this.bdd = Room.databaseBuilder(context, ConnectorDB.class, "judotracker").build();
@@ -21,4 +24,51 @@ public class LocalDatabase {
         return bdd.competitionDao().getAll();
     }
 
+    public List<Adversaire> getAllAdversaires() {
+        return bdd.adversaireDao().getAll();
+    }
+
+    public List<Categorie> getAllCategories() {
+        return bdd.categorieDao().getAll();
+    }
+
+    public List<Localisation> getAllLocalisations() {
+        return bdd.localisationDao().getAll();
+    }
+
+    public List<Match> getAllMatchs() {
+        return bdd.matchDao().getAll();
+    }
+
+    public List<Statistiques> getAllStatistiques() {
+        return bdd.statistiquesDao().getAll();
+    }
+
+    public Adversaire getAdversaire(int idAdversaire) {
+        return bdd.adversaireDao().getById(idAdversaire);
+    }
+
+    public Adversaire getAdversaireByMatch(Match match) {
+        return bdd.adversaireDao().getById(match.getId_adversaire());
+    }
+
+    public Categorie getCategorie(int idCategorie) {
+        return bdd.categorieDao().getById(idCategorie);
+    }
+
+    public List<Categorie> getCategorieBySexe(String sexe) {
+        return bdd.categorieDao().getBySexe(sexe);
+    }
+
+    public Competition getCompetition(int idCompetition) {
+        return bdd.competitionDao().getById(idCompetition);
+    }
+
+    public List<Competition> getCompetitionsByLocalisation(Localisation localisation) {
+        return bdd.competitionDao().getByIdLocalisation(localisation.getIdLocalisation());
+    }
+
+    public List<Competition> getCompetitionsByCategorie(Categorie categorie) {
+        return bdd.competitionDao().getByIdCategorie(categorie.getIdCategorie());
+    }
 }
